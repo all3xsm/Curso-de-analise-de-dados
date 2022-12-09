1f735612d007e12b5b57b617ff8454cf6b6ea318
all3xsm <112523623+all3xsm@users.noreply.github.com>
2022-12-09 02:53:15 +0000

    Senha

---
+++ Questao3.py
@@ -0,0 +1,8 @@
+acesso = int(input('Digite a senha: '))
+
+while acesso != 2002:
+    print('Senha inválida.')
+    acesso = int(input('Digite a senha: '))
+
+print('Acesso permitido.')
+print('Fim do programa.')
