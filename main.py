71ed3fed5afb7d41aa21062b1d74b2546f59ff3b
all3xsm <112523623+all3xsm@users.noreply.github.com>
2022-12-14 02:32:16 +0000

    Atividade 2

--- main.py
+++ main.py
@@ -1,1 +1,67 @@
-print('Olá mundo')
+from BancoLib import Banco
+
+print("Bem-vindo")
+bancoUfrpe = Banco("UABJ")
+print("Menu")
+print("0 - Sair")
+print("1 - Criar uma Nova Conta")
+print("2 - Render Bonificada")
+print("3 - Consultar Saldo Conta")
+print("4 - Depositar na Conta")
+print("5 - Sacar na Conta")
+print("6 - Render Poupanca")
+escolha = int(input("digite a opção desejada:"))
+while escolha > 0:
+    if escolha == 1:
+        # criar uma conta
+        print("Criando Conta...")
+        print("1 - Conta Corrente")
+        print("2 - Conta Poupanca")
+        print("3 - Conta Bonificada")
+        opcao = int(input("digite o tipo da conta:"))
+        if opcao == 1:
+            numConta = bancoUfrpe.criarConta()
+        elif opcao == 2:
+            numConta = bancoUfrpe.criarPoupanca()
+        else:
+            numConta = bancoUfrpe.criarContaBonificada()
+        print("Conta criada:", numConta)
+
+    elif escolha == 2:
+        print("Conta Bonificada...")
+        numConta = int(input("digite o numero da conta bonificada:"))
+        resp = bancoUfrpe.renderBonificada(numConta)
+        if resp:
+            print("Conta bonificada com novo saldo")
+        else:
+            print("A conta não é bonificada ou não existe")   
+    elif escolha == 3:
+        print("Consultando Saldo...")
+        numConta = int(input("digite o numero da conta:"))
+        saldo = bancoUfrpe.consultaSaldo(numConta)
+        print("o saldo da conta", numConta, "é", saldo, "R$")
+    elif escolha == 4:
+        print("Depositando Conta...")
+        numConta = int(input("digite o numero da conta:"))
+        valor = int(input("digite o valor que deseja depositar:"))
+        saldo = bancoUfrpe.depositar(numConta, valor)
+        print("Valor Depositado")
+    elif escolha == 5:
+        print("Sacando da Conta...")
+        numConta = int(input("digite o numero da conta:"))
+        valor = int(input("digite o valor que deseja sacar:"))
+        resp = bancoUfrpe.sacar(numConta, valor)
+        if resp:  # significa resp == True
+            print("Valor Sacado")
+        else:
+            print("Saldo Insuficiente")
+    elif escolha == 6:
+        print("Rendendo Poupanca...")
+        numConta = int(input("digite o numero da conta poupanca:"))
+        resp = bancoUfrpe.renderPoupanca(numConta)
+        if resp:
+            print("Poupanca com novo saldo")
+        else:
+            print("A conta não é poupanca ou não existe")
+    
+    escolha = int(input("digite a opção desejada:"))
